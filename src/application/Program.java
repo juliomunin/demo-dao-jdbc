34fc974b7714ddac1f6b0764425e2097ed9b7434
unknown <jfmunin@gmail.com>
2019-11-03 21:50:02 -0300

    Implementando FindByDepartament

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import model.entities.Departament;
 import model.entities.Seller;
 import modeo.dao.DaoFactory;
 import modeo.dao.SellerDao;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -12,9 +14,17 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== Teste nº1: Selle foundById ====");
+        System.out.println("=== Teste nº1: Seller findById ====");
         Seller seller = sellerDao.findById(1);
         System.out.println(seller);
+
+        System.out.println("\n=== Teste nº2: Departament findById ====");
+        Departament departament = new Departament(2, null);
+        List<Seller> list = sellerDao.findByDepartment(departament);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
 
     }
 }
--- src/modeo/dao/SellerDao.java
+++ src/modeo/dao/SellerDao.java
@@ -1,6 +1,8 @@
 package modeo.dao;
 
 import java.util.List;
+
+import model.entities.Departament;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -10,6 +12,7 @@ public interface SellerDao {
     void deleteById(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
+    List<Seller> findByDepartment(Departament departament);
 
     }
 
