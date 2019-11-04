5d0561555a6d8caa2b670aa3bbf1ae2fd214d6d4
unknown <jfmunin@gmail.com>
2019-11-03 22:09:53 -0300

    FinAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,11 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println("\n=== Teste nº3: Departament findAll ====");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
 
     }
 }
